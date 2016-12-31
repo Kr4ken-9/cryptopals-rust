fd753a4f7c3619d92cb7b6120194a603a69e6a79
Regnar-Kel <regnar.kel@gmail.com>
2016-12-30 19:25:18 -0500

    Refactoring

--- DebugSnippet.rs
+++ DebugSnippet.rs
@@ -1,33 +1,40 @@
 use std::str;
 
 fn main() {
-    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    println!("{}", bytes_to_base64(hex_to_bytes("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")));
+}
+
+fn hex_to_bytes(hex: &str) -> Vec<u8> {
     let hex_chars = hex.chars();
     let mut byte_array_vector: Vec<u8> = Vec::with_capacity(48 as usize);
     let mut is_high = true;
     let mut current_value:u8 = 0;
-    let mut current_bit = 0;
-    let mut current_base64:u8 = 0;
-    let mut base_64 = String::with_capacity(64);
-    //let base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-
     for x in hex_chars {
         let byte_val = match x {
-            '0'...'9' => (x as u8) - 48,
-            'a'...'f' => (x as u8) - 87,
+            '0'...'9' => (x as i16) - 48,
+            'a'...'f' => (x as i16) - 87,
             _ => panic!("Invalid character!"),
         };
         if is_high {
-            current_value = byte_val << 4;
+            current_value = (byte_val as u8) << 4;
         } else {
-            byte_array_vector.push(current_value | byte_val);
+            byte_array_vector.push(current_value | (byte_val as u8));
         }
         
         is_high = !is_high;
     }
-    for z in byte_array_vector {
+
+    return byte_array_vector;
+}
+
+fn bytes_to_base64(bytes: Vec<u8>) -> String {
+    let mut current_bit = 0;
+    let mut current_base64:u8 = 0;
+    let mut base_64 = String::with_capacity(64);
+
+    for x in bytes {
         for i in 0..8 {
-            let y = (z >> (7 - i)) & 0x01;
+            let y = (x >> (7 - i)) & 0x01;
 
             if current_bit == 0 {
                 current_base64 = y; 
@@ -49,19 +56,5 @@ fn main() {
             }
         }
     }
-    println!("{}", base_64);
+    return base_64;
 }
-
-/*
-fn Hex_To_Bytes(hex){
-    let hex_chars = hex.chars();
-    for x in hex_chars {
-        let byte_val = match x {
-            '0'...'9' => (x as i16) - 48,
-            'a'...'f' => (x as i16) - 87,
-            _ => panic!("Invalid character!"),
-        };
-        println!("Literal {}", byte_val);
-    }
-}
-*/
